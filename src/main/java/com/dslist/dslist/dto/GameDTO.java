f65c0c7cddad1e0be3706bf27095dcfd6b7b14e8
Theyvison <theyvison.jf@hotmail.com>
2024-11-12 12:20:10 -0300

    GET game by id

---
+++ src/main/java/com/dslist/dslist/dto/GameDTO.java
@@ -0,0 +1,87 @@
+package com.dslist.dslist.dto;
+
+import com.dslist.dslist.entities.Game;
+import org.springframework.beans.BeanUtils;
+
+public class GameDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String platforms;
+    private Double score;
+    private String imageUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameDTO() {
+
+    }
+
+    public GameDTO(Game entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+}
